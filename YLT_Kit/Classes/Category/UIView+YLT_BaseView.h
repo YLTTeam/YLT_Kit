27ed306baef121a52a2bbe5763ce24f5799e5c73
相公 <xianggong@anve.com>
2018-04-13 14:01:51 +0800

    no message

--- YLT_Kit/Classes/Category/UIView+YLT_BaseView.h
+++ YLT_Kit/Classes/Category/UIView+YLT_BaseView.h
@@ -39,4 +39,46 @@
  */
 @property (nonatomic, assign) CGFloat ylt_centerY;
 
+/**
+ 坐标 x, y点
+ */
+@property (nonatomic, assign) CGPoint ylt_origin;
+
+/**
+ 宽高 width, height
+ */
+@property (nonatomic, assign) CGSize ylt_size;
+
+/**
+ 视图的 顶点 min_y
+ */
+@property (nonatomic, assign) CGFloat ylt_top;
+
+/**
+ 视图的 底点 max_y
+ */
+@property (nonatomic, assign) CGFloat ylt_bottom;
+
+/**
+ 视图的 左边 min_x
+ */
+@property (nonatomic, assign) CGFloat ylt_left;
+
+/**
+ 视图的 右边 max_x
+ */
+@property (nonatomic, assign) CGFloat ylt_right;
+
+/**
+ 移除所有的子视图
+ */
+- (void)removeAllSubViews;
+
+/**
+ 移除指定的所有子视图
+ 
+ @param className 子视图类名字符串
+ */
+- (void)removeAllSubviewsClass:(NSString *)className;
+
 @end
--- YLT_Kit/Classes/Category/UIView+YLT_BaseView.m
+++ YLT_Kit/Classes/Category/UIView+YLT_BaseView.m
@@ -9,8 +9,7 @@
 
 @implementation UIView (YLT_BaseView)
 
-@dynamic ylt_centerX;
-@dynamic ylt_centerY;
+@dynamic ylt_top, ylt_bottom, ylt_left, ylt_right;
 
 - (void)setYlt_x:(CGFloat)x {
     self.frame = CGRectMake(x, self.ylt_x, self.ylt_width, self.ylt_height);
@@ -48,9 +47,96 @@
     return (self.ylt_x+self.ylt_width)/2.;
 }
 
+- (void)setYlt_centerX:(CGFloat)ylt_centerX {
+    self.center = CGPointMake(ylt_centerX, self.center.y);
+}
+
 - (CGFloat)ylt_centerY {
     return (self.ylt_y+self.ylt_height)/2.;
 }
 
+- (void)setYlt_centerY:(CGFloat)ylt_centerY {
+    self.center = CGPointMake(self.center.x, ylt_centerY);
+}
+
+/**
+ 坐标 x, y点
+ */
+- (CGPoint)ylt_origin {
+    return CGPointMake(self.ylt_x, self.ylt_y);
+}
+
+- (void)setYlt_origin:(CGPoint)ylt_origin {
+    CGRect frame = self.frame;
+    frame.origin = ylt_origin;
+    self.frame = frame;
+}
+
+/**
+ 宽高 width, height
+ */
+- (CGSize)ylt_size {
+    return CGSizeMake(self.ylt_width, self.ylt_height);
+}
+
+- (void)setYlt_size:(CGSize)ylt_size {
+    CGRect frame = self.frame;
+    frame.size = ylt_size;
+    self.frame = frame;
+}
+
+/**
+ 视图的 顶点 min_y
+ */
+- (CGFloat)ylt_top {
+    return self.ylt_y;
+}
+
+/**
+ 视图的 底点 max_y
+ */
+- (CGFloat)ylt_bottom {
+    return (self.ylt_y+self.ylt_height);
+}
+
+/**
+ 视图的 左边 min_x
+ */
+- (CGFloat)ylt_left {
+    return self.ylt_x;
+}
+
+/**
+ 视图的 右边 max_x
+ */
+- (CGFloat)ylt_right {
+    return (self.ylt_x+self.ylt_width);
+}
+
+/**
+ 移除所有的子视图
+ */
+- (void)removeAllSubViews {
+    for (id view in self.subviews) {
+        [view removeFromSuperview];
+    }
+}
+
+/**
+ 移除指定的所有子视图
+
+ @param className 子视图类名字符串
+ */
+- (void)removeAllSubviewsClass:(NSString *)className {
+    if ([className length] == 0 || className == nil) {
+        [self removeAllSubViews];
+        return;
+    }
+    for (UIView *view in self.subviews) {
+        if ([view isKindOfClass:NSClassFromString(className)]) {
+            [view removeFromSuperview];
+        }
+    }
+}
 
 @end
