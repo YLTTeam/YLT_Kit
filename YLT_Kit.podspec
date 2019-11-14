4e6082b23d0778dc29f35bfa95af44b6a6a71732
项工 <xiangpuhua@126.com>
2019-11-14 15:16:27 +0800

    release 0.3.52

--- YLT_Kit.podspec
+++ YLT_Kit.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'YLT_Kit'
-  s.version          = '0.3.51'
+  s.version          = '0.3.52'
   s.summary          = 'YLT_Kit. 基础的kit框架'
 
   s.description      = <<-DESC
--- YLT_Kit/Classes/Category/UIView+YLT_Extension.m
+++ YLT_Kit/Classes/Category/UIView+YLT_Extension.m
@@ -89,14 +89,16 @@
  @param radius 圆角率
  */
 - (void)ylt_cornerType:(UIRectCorner)rectCorner radius:(NSUInteger)radius {
-    //绘制圆角 要设置的圆角 使用“|”来组合
-    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.bounds byRoundingCorners:rectCorner cornerRadii:CGSizeMake(radius, radius)];
-    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
-    //设置大小
-    maskLayer.frame = self.bounds;
-    //设置图形样子
-    maskLayer.path = maskPath.CGPath;
-    self.layer.mask = maskLayer;
+    YLT_MAINDelay(0.1, ^{
+        //绘制圆角 要设置的圆角 使用“|”来组合
+        UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.bounds byRoundingCorners:rectCorner cornerRadii:CGSizeMake(radius, radius)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+        //设置大小
+        maskLayer.frame = self.bounds;
+        //设置图形样子
+        maskLayer.path = maskPath.CGPath;
+        self.layer.mask = maskLayer;
+    });
 }
 
 - (void)ylt_borderWidth:(CGFloat)width
