5c75f0b5e22863d6aafa031475d15495f32c9a40
Alex <xphaijj0305@126.com>
2020-05-02 09:10:22 +0800

    no message

--- YLT_Kit/Classes/Category/UIImage+YLT_Extension.m
+++ YLT_Kit/Classes/Category/UIImage+YLT_Extension.m
@@ -351,7 +351,7 @@ static CGBitmapInfo const kTCDefaultBitMapOrder = kCGBitmapByteOrder32Little | k
 + (UIImage *)ylt_convertViewToImage:(UIView *)view {
     //第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了
     UIGraphicsBeginImageContextWithOptions(view.bounds.size, YES, [UIScreen mainScreen].scale);
-    [view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:YES];
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return image;
