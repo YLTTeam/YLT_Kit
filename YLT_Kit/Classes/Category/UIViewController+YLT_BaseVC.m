183f34cbbcf2c72b409dc3a086c207bde6fbc28b
项工 <xiangpuhua@126.com>
2020-06-13 11:24:18 +0800

    release 0.4.6

--- YLT_Kit/Classes/Category/UIViewController+YLT_BaseVC.m
+++ YLT_Kit/Classes/Category/UIViewController+YLT_BaseVC.m
@@ -30,18 +30,6 @@
 
 #pragma mark - hook
 - (void)ylt_viewDidLoad {
-    if ([self respondsToSelector:@selector(ylt_bindData)]) {
-        [self performSelector:@selector(ylt_bindData)];
-    }
-    if ([self respondsToSelector:@selector(ylt_setup)]) {
-        [self performSelector:@selector(ylt_setup)];
-    }
-    if ([self respondsToSelector:@selector(ylt_addSubViews)]) {
-        [self performSelector:@selector(ylt_addSubViews)];
-    }
-    if ([self respondsToSelector:@selector(ylt_request)]) {
-        [self performSelector:@selector(ylt_request)];
-    }
     [self ylt_viewDidLoad];
 }
 
